4425f2e35f3f342fa0ed767ab6394d47d68b0aa2
kumar <lesakkimuthukumar20@gmail.com>
2021-12-18 19:59:50 +0530

    complete

--- task/src/app/components/table/table.component.ts
+++ task/src/app/components/table/table.component.ts
@@ -10,58 +10,137 @@ import {client , campaign} from '../dto/Datas.dto';
 import { HttpClient } from '@angular/common/http';
 import { Router, Navigation } from '@angular/router';
 import { async } from 'rxjs';
+import { MatDrawer } from '@angular/material/sidenav';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent implements OnInit {
+export class TableComponent {
       
       dashboardlist: any;
-      
       column: string[] = ['Name','phone','company','invitation','status','created','updated','Action'];   
       column1: string[] = ['Name','Description','client','listid','status','created','EndDate','updated','Action'];
    
       clientlist = new MatTableDataSource(); // Client Tablex
       campaignslist = new MatTableDataSource();  // Campaigns Table
-  
+       
+      arr1 : any[] = [];
+      arr2 : any[] = [];
+      arr3 : any[] = [];
+      arr4 : any[] = [];
+     
   constructor(private service : HttpService,private Tservice : TableService,private http:HttpClient) 
   {    
-      
-  }
-  ngOnInit(){
     this.service._refreshNeeds.subscribe(() => {
-        this.getdash();
-        this.getclient();
-        this.getcampaign();
+      this.getclient();
+      this.getcampaign();
     })
-     this.getdash();
-     this.getclient();
-     this.getcampaign();
+    this.getclient();
+    this.getcampaign();
   }
-  getdash(){
-    this.service.getdashboard().subscribe( (response)=>{
-      this.dashboardlist = JSON.parse(JSON.stringify(response[0]));
-      console.log(this.dashboardlist);
-    });
+  execute = false;
+  // getdash(){
+  //   // this.service.getdashboard().subscribe( (response)=>{
+  //   //   this.dashboardlist = JSON.parse(JSON.stringify(response[0]));
+  //   //   console.log(this.dashboardlist);
+  //   // });
+  // } 
+getdash(){
+ var acount:number=0;
+ var icount:number=0;
+
+ var cacount:number=0; 
+ var iacount: number=0;
+  var activeClients:any;
+  var inactiveClients:any;
+  var completedCampaigns:any;
+  var assignedCampaigns:any;
+  var len = this.arr2.length;
+  var len1 = this.arr3.length;
+  // console.log(len);
+  for(let i = 0; i <= len-1; i++){
+    console.log("hi");
+      // if( i == 2){
+      // console.log(this.arr2[i].is_deleted);
+      // }
+      if(this.arr2[i]?.is_deleted == true){
+        // console.log(i+3);
+        // console.log(true);
+        acount++; 
+        // console.log(this.acount);
+      } else {
+        // console.log(false);
+        icount++;
+        // console.log(this.icount);
+      }
   }
+  activeClients = acount;
+  inactiveClients = icount;
+  console.log(activeClients);
+  console.log(inactiveClients);
+
+  for(let i = 0; i <= len1-1; i++){
+    console.log("hi");
+      // if( i == 2){
+      // console.log(this.arr2[i].is_deleted);
+      // }
+      if(this.arr3[i]?.is_deleted == true){
+        // console.log(i+3);
+        // console.log(true);
+        cacount++; 
+        // console.log(this.acount);
+      } else {
+        // console.log(false);
+        iacount++;
+        // console.log(this.icount);
+      }
+  }
+  assignedCampaigns = cacount;
+  completedCampaigns= iacount;
+  console.log(assignedCampaigns);
+  console.log(completedCampaigns);
+  this.dashboardlist = {
+    activeClients : activeClients,
+    inactiveClients : inactiveClients,
+    assignedCampaigns:assignedCampaigns,
+    completedCampaigns : completedCampaigns
+  }
+  
+}
+
 getcampaign(){
   this.service.getcampaign().subscribe( (response)=>{
     let  data1 = response;
-    this.campaignslist = new MatTableDataSource(data1)
+    this.arr1 = response;
+    this.getArr();
+    this. getdash();
+    // this.once1();
+    this.campaignslist = new MatTableDataSource(this.arr3);
     this.campaignslist.paginator = this.paginator.toArray()[1];
+    
   })
 }
 getclient(){
 this.service.getclient().subscribe((response)=>{
   let data : any =response;
-  this.clientlist = new MatTableDataSource(data)
+  this.arr2 = response;
+  this.clientlist = new MatTableDataSource(data);
   this.clientlist.paginator = this.paginator.toArray()[0];
  });
 }
+ getArr(){
+  console.log(this.arr1);
+  console.log(this.arr2);
+  this.arr3 = this.arr1.filter( (cam) => {return this.arr2.some((cl) => {  if(cam.client.oid === cl._id.oid)
+  { 
+    console.log(typeof cam.client)
+    return  cam.client = cl.name
+  } }) });
+  console.log(this.arr3);
+ }
 StatusRetClient(ids:any,stat:any){
   this.service.putclientdel(ids,stat).subscribe((response)=>{
-      // console.log(response);
   })
 }
 StatusCamp(ids:any,status:any){
@@ -86,24 +165,24 @@ deleteData(ids:any){
   is_deleted: boolean = true;
 
   // hover Effect Delete And Restore Icon button
-  kumar = "btn-mat-icon delete-btn";
+  kumar = " delete-btn";
   kumar1 = "restore-btn";
   //  refresh() {
   //    window.location.reload()
   //  }
-  del(id:any,stat:any){ 
+  del(id:any,stat:any,ele:any){ 
     // status retire
     this.StatusRetClient(id,stat);
-    this.kumar = "disab"                      // button hover class
-    this.kumar1 = "restore-btn"; 
+    ele.kumar = "disab";                      // button hover class
+    ele.kumar1 = "restore-btn"; 
     // this.refresh()
   }
 
-  ret(id:any,stat:any){
+  ret(id:any,stat:any,ele:any){
     // status retire
     this.StatusRetClient(id,stat);
-    this.kumar = "btn-mat-icon delete-btn";   // button hover Class 
-    this.kumar1 = "disab1";
+    ele.kumar = "delete-btn";   // button hover Class 
+    ele.kumar1 = "disab1";
     // this.refresh();
   };
   check =true;
@@ -129,9 +208,6 @@ deleteData(ids:any){
     });
   onSubmit() 
   {
-    //  console.log(this.is_deleted);
-    // this.profileForm.value.phone.toString();
-    //  console.log(this.createDate);
      if(this.profileForm.valid)
      {
        var payloadclient = this.profileForm.value;
@@ -147,13 +223,12 @@ deleteData(ids:any){
           created_at,
           updated_at,
         }
-        // console.log(payload);
-        // this.service.postclient(payload);
         this.service.postclient(payload).subscribe((res)=>{
          console.log(res);
        })
      }
      this.profileForm.reset();
+    
   } 
 
   copyfunc(Text:any) 
@@ -164,7 +239,6 @@ deleteData(ids:any){
     },)
    }
   }
-
 
 
 
